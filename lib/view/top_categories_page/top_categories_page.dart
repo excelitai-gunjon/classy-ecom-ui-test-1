3196c391f52c9a235c5c95dc0ed64a95c1f1a8b5
excelitai-gunjon <gunjon@excelitai.com>
2022-01-17 16:32:31 +0600

    top categories

---
+++ lib/view/top_categories_page/top_categories_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TopCategoriesPage extends StatelessWidget {
+  const TopCategoriesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 60,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}

a32ba8ddcab8a52af9e2341405c000d4ff269f2e
excelitai-gunjon <gunjon@excelitai.com>
2022-04-25 13:11:14 +0600

    fashion final

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context){
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: PrimaryScreenState()),
